fffaf73539cacfffa38f60692b73a90e3cb085cd
Antoine07 <antoine.lucsko@gmail.com>
2023-05-11 15:02:21 +0200

    correc

---
+++ tp_correction.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+
+names = pd.read_csv('data/names/yob1880.txt', names=['name', 'sex', 'birth'])
+
+# 1. Nb de naissances
+grouped = names.groupby('sex')['birth']
+
+print(grouped.sum())
+
+# 2. Concat yobs files
+columns = ['name', 'sex', 'birth']
+
+yobs = []
+
+
+for year in range(1880, 2019):
+    path = "data/names/yob{}.txt".format(year)
+    df = pd.read_csv(path, names=columns)
+    df['year'] = year
+    yobs.append(df)
+
+names = pd.concat(yobs, ignore_index=True)
+
+names.head()
+
+total_births = names.pivot_table(values='birth', index='year', columns='sex', aggfunc=sum)[:10]
+
+total_births.plot()
